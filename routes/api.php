b3b224c26f80ae8061c4dd92113af64e5619934f
ralfaro <rodrigo.alfaro@salud.gob.sv>
2026-03-25 15:28:55 -0600

    Add 'is_active' field to selected posts in getAllPostByUser method and update delete route to use DELETE method

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::middleware('api.key')->group(function(){
             Route::get('/users',[UserController::class,'getAllUsers']);
             Route::get('/{id}',[UserController::class, 'getUserById']);
             Route::patch('/update/{id}',[UserController::class, 'updateUser']);
-            Route::patch('/delete/{id}', [UserController::class,'deleteUser']);
+            Route::delete('/delete/{id}', [UserController::class,'deleteUser']);
         });
     });
     Route::prefix('v1/blog')->group(function(){
@@ -26,7 +26,7 @@ Route::middleware('api.key')->group(function(){
             Route::post('/create',[BlogPostController::class, 'createBlogPost']);
             Route::get('/user_posts/{id}', [BlogPostController::class, 'getAllPostByUser']);
             Route::patch('/update/{id}',[BlogPostController::class, 'updatePost']);
-            Route::patch('/delete/{id}',[BlogPostController::class, 'deletePost']);
+            Route::delete('/delete/{id}',[BlogPostController::class, 'deletePost']);
         });
     });
 
